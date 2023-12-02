2157dbbfbdbad8496c8a6c7629a757c08b046fba
ayoadereti <132008328+ayoadereti@users.noreply.github.com>
2023-12-02 13:33:33 -0600

    Add files via upload

---
+++ src/permit + account/test/UserTest.java
@@ -0,0 +1,100 @@
+package AccountMngmt.entity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    User u;
+
+    @BeforeEach
+    void setUp() {
+        u = new User();
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void allValid() {
+        u.setFirstName("Ayobami");
+        u.setLastName("Adereti");
+        u.setEmail("ara190013@utdallas.edu");
+        u.setPassword("ohYeah!");
+        u.setPermit("testPermit");
+
+        assertTrue(u.validateFirstName("Ayobami"));
+        assertTrue(u.validateLastName("Adereti"));
+        assertTrue(u.validateEmail("ara190013@utdallas.edu"));
+        assertTrue(u.validatePassword("ohYeah!"));
+        assertTrue(u.validatePermit("testPermit"));
+    }
+
+    @Test
+    void invalidName() {
+        u.setFirstName("Ayobam!");
+        u.setLastName("Aderet i");
+        u.setEmail("ara190013@utdallas.edu");
+        u.setPassword("ohYeah!");
+        u.setPermit("testPermit");
+
+        assertFalse(u.validateFirstName("Ayobam!"));
+        assertFalse(u.validateLastName("Aderet i"));
+        assertTrue(u.validateEmail("ara190013@utdallas.edu"));
+        assertTrue(u.validatePassword("ohYeah!"));
+        assertTrue(u.validatePermit("testPermit"));
+    }
+
+    @Test
+    void invalidEmail() {
+        u.setFirstName("Ayobami");
+        u.setLastName("Adereti");
+        u.setEmail("ara19o013@utdallas.edu");
+        u.setPassword("ohYeah!");
+        u.setPermit("testPermit");
+
+        assertTrue(u.validateFirstName("Ayobami"));
+        assertTrue(u.validateLastName("Adereti"));
+        assertFalse(u.validateEmail("ara19o013@utdallas.edu"));
+        assertTrue(u.validatePassword("ohYeah!"));
+        assertTrue(u.validatePermit("testPermit"));
+    }
+
+    @Test
+    void invalidPassword() {
+        u.setFirstName("Ayobami");
+        u.setLastName("Adereti");
+        u.setEmail("ara190013@utdallas.edu");
+        u.setPassword("ohNo");
+        u.setPermit("testPermit");
+
+        assertTrue(u.validateFirstName("Ayobami"));
+        assertTrue(u.validateLastName("Adereti"));
+        assertTrue(u.validateEmail("ara190013@utdallas.edu"));
+        assertFalse(u.validatePassword("ohNo"));
+        assertTrue(u.validatePermit("testPermit"));
+    }
+
+    @Test
+    void invalidPermit() {
+        u.setFirstName("Ayobami");
+        u.setLastName("Adereti");
+        u.setEmail("ara190013@utdallas.edu");
+        u.setPassword("ohYeah!");
+        u.setPermit("bad Permit");
+
+        assertTrue(u.validateFirstName("Ayobami"));
+        assertTrue(u.validateLastName("Adereti"));
+        assertTrue(u.validateEmail("ara190013@utdallas.edu"));
+        assertTrue(u.validatePassword("ohYeah!"));
+        assertFalse(u.validatePermit("bad Permit"));
+    }
+
+
+
+}
