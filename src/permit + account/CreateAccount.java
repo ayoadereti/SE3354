26e8f6a4f6aefb24b9011ad68615a6ec0395af5c
ayoadereti <132008328+ayoadereti@users.noreply.github.com>
2023-12-02 14:27:08 -0600

    Update CreateAccount.java

--- src/permit + account/CreateAccount.java
+++ src/permit + account/CreateAccount.java
@@ -11,6 +11,7 @@ public class CreateAccount {
         Scanner scnr = new Scanner(System.in);
 
         while (repeat) {
+            // Get account info from user input
             System.out.println("First Name: ");
             String firstName = scnr.next();
 
@@ -23,10 +24,12 @@ public class CreateAccount {
             System.out.println("Password: ");
             String password = scnr.next();
 
+            // Initialize an Account Controller obj and call the createAccount method
             AccountController controller = new AccountController();
             String message = controller.createAccount(firstName, lastName, email, password).print();
             System.out.println("\n" + message);
 
+            // Prompt user to try again if account info is invalid 
             if (!message.contains("Invalid")) {
                 repeat = false;
             }
