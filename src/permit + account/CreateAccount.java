448b5c797d1bcbf423cc4b083696dec01816faab
ayoadereti <132008328+ayoadereti@users.noreply.github.com>
2023-12-02 14:17:36 -0600

    Update CreateAccount.java

--- src/permit + account/CreateAccount.java
+++ src/permit + account/CreateAccount.java
@@ -7,22 +7,29 @@ import java.util.Scanner;
 
 public class CreateAccount {
     public static void main(String[] args) {
+        boolean repeat = true;
         Scanner scnr = new Scanner(System.in);
 
-        System.out.println("First Name: ");
-        String firstName = scnr.next();
+        while (repeat) {
+            System.out.println("First Name: ");
+            String firstName = scnr.next();
 
-        System.out.println("Last Name: ");
-        String lastName = scnr.next();
+            System.out.println("Last Name: ");
+            String lastName = scnr.next();
 
-        System.out.println("UTD email (username): ");
-        String email = scnr.next();
+            System.out.println("UTD email (username): ");
+            String email = scnr.next();
 
-        System.out.println("Password: ");
-        String password = scnr.next();
+            System.out.println("Password: ");
+            String password = scnr.next();
 
-        AccountController controller = new AccountController();
-        System.out.println(controller.createAccount(firstName, lastName, email, password).print());
+            AccountController controller = new AccountController();
+            String message = controller.createAccount(firstName, lastName, email, password).print();
+            System.out.println("\n" + message);
 
+            if (!message.contains("Invalid")) {
+                repeat = false;
+            }
+        }
     }
 }
